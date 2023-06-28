3e0ca4e399af28db224cc6f2518f1d69fc041fe8
JerrySeazia <jerrykoiyanseazia@gmail.com>
2023-06-27 19:43:08 -0700

    sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,82 @@
+
+#include "sort.h"
+
+void swap_ints(int *c, int *d);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+/**
+ * swap_ints - function of the prototype
+ * @c: The first swap.
+ * @d: The second swap.
+ */
+void swap_ints(int *c, int *d)
+{
+int temp;
+temp = *c;
+*c = *d;
+*d = temp;
+}
+
+/**
+ * lomuto_partition - function of the prototype
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+int *pivot, up, down;
+pivot = array + right;
+for (up = down = left; down < right; down++)
+{
+if (array[down] < *pivot)
+{
+if (up < down)
+{
+swap_ints(array + down, array + up);
+print_array(array, size);
+}
+up++;
+}
+}
+if (array[up] > *pivot)
+{
+swap_ints(array + up, pivot);
+print_array(array, size);
+}
+return (up);
+}
+/**
+ * lomuto_sort - function of the prototype
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+int parts;
+if (right - left > 0)
+{
+parts = lomuto_partition(array, size, left, right);
+lomuto_sort(array, size, left, parts - 1);
+lomuto_sort(array, size, parts + 1, right);
+}
+}
+/**
+ * quick_sort - function of the prototype
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+if (array == NULL || size < 2)
+return;
+lomuto_sort(array, size, 0, size - 1);
+}
